6189685126f809d40237eecfec4ea501f9292bfe
shukubota <kubotashu0421@gmail.com>
2022-02-23 19:24:10 +0900

    conflict修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:ohsho_shutsujin/pages/panel.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const OhshoShutsujin());
+  runApp(ProviderScope(child: const OhshoShutsujin()));
 }
 
-class OhshoShutsujin extends HookWidget {
+class OhshoShutsujin extends HookConsumerWidget {
   const OhshoShutsujin({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: '王将出陣',
       theme: ThemeData(
