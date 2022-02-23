41cf930d254ee21a89873255189245a1ff22522b
shukubota <kubotashu0421@gmail.com>
2022-02-23 19:35:03 +0900

    form_example追加

---
+++ lib/pages/form_example.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class FormExample extends HookWidget {
+  const FormExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('aaa'),
+    );
+  }
+}
