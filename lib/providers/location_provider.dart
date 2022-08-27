0d0c1fb782b2d61ee144ee2377455d162e26de30
shukubota <kubotashu0421@gmail.com>
2022-08-27 14:58:43 +0900

    providerで状態管理

--- lib/providers/location_provider.dart
+++ lib/providers/location_provider.dart
@@ -1,3 +1,46 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:ohsho_shutsujin/model/koma.dart';
+
+final komaMapProvider = StateNotifierProvider<KomaMapController, List<Koma>>((ref) => KomaMapController());
+
+final ohsho = Koma(id: 1, komaType: KomaType.ohsho, point: Point(x: 0, y: 0), title: "王");
+final kin = Koma(id: 2, komaType: KomaType.kin, point: Point(x: 2, y: 2), title: "金");
+final List<Koma> initialKomaList = [
+  ohsho,
+  kin,
+];
+
+// コマの配置を管理する
+class KomaMapController extends StateNotifier<List<Koma>> {
+  KomaMapController(): super(initialKomaList);
+
+  void changeKomaPointToRight(Koma koma) {
+    final point = koma.point;
+    final newPoint = Point(x: point.x + 1, y: point.y);
+
+    print(koma.height);
+    print(koma.width);
+    print("------9999");
+    print(koma.komaType);
+    print(koma.title);
+    print(koma.id);
+    print(state[0].id);
+    print(koma.id == state[0].id);
+    print(state[1].id);
+    print("------9999");
+    state = [
+      for (final currentKoma in state)
+        if (koma.id == currentKoma.id)
+          Koma(id: koma.id, komaType: koma.komaType, title: koma.title, point: newPoint)
+        else
+          currentKoma,
+    ];
+    print(state);
+    print(state[0].title);
+    print(state[1].title);
+  }
+}
+
 // x: 0-3
 // y: 0-4
 class Location {
