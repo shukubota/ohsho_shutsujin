1ead8e0f0c8ff964ac6229f0243c048166af9c01
shukubota <kubotashu0421@gmail.com>
2022-08-28 00:27:31 +0900

    KomaMapを定義

--- lib/providers/location_provider.dart
+++ lib/providers/location_provider.dart
@@ -1,7 +1,9 @@
+import 'package:collection/src/iterable_extensions.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ohsho_shutsujin/model/koma.dart';
+import 'package:ohsho_shutsujin/pages/panel.dart';
 
-final komaMapProvider = StateNotifierProvider<KomaMapController, List<Koma>>((ref) => KomaMapController());
+final komaMapProvider = StateNotifierProvider<KomaMapController, KomaMap>((ref) => KomaMapController());
 
 final ohsho = Koma(id: 1, komaType: KomaType.ohsho, point: Point(x: 0, y: 0), title: "王");
 final kin = Koma(id: 2, komaType: KomaType.kin, point: Point(x: 2, y: 2), title: "金");
@@ -10,34 +12,72 @@ final List<Koma> initialKomaList = [
   kin,
 ];
 
+class KomaMap {
+  final List<Koma> komaList;
+  KomaMap({ required this.komaList });
+
+  bool canToRight(Koma koma) {
+    print(koma.point.x);
+    print(koma.point.y);
+    print("point=======");
+    // 境界値チェック
+    if (koma.point.x + koma.width >= panelCountX) {
+      return false;
+    }
+
+    final filledPointList = getFilledPointList();
+    // 右が空いているかチェック
+    for (int i = 0; i < koma.height; i++) {
+      final destination = Point(x: koma.point.x + koma.width, y: koma.point.y);
+      // 行き先候補が埋まっていればfalseを返す
+      final existing = filledPointList.firstWhereOrNull((e) => e.isEqual(destination));
+      if (existing != null) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  List<Point> getFilledPointList() {
+    var _filledPointList = <Point>[];
+    for (int i = 0; i < komaList.length; i++) {
+      final Koma koma = komaList[i];
+      final pointList = koma.getPointList();
+      for (int j = 0; j < pointList.length; j++) {
+        final point = pointList[j];
+        final existing = _filledPointList.firstWhereOrNull((e) => e.isEqual(point));
+        if (existing != null) {
+          _filledPointList.add(point);
+        }
+      }
+    }
+    return _filledPointList;
+  }
+}
+
 // コマの配置を管理する
-class KomaMapController extends StateNotifier<List<Koma>> {
-  KomaMapController(): super(initialKomaList);
+class KomaMapController extends StateNotifier<KomaMap> {
+  KomaMapController(): super(KomaMap(komaList: initialKomaList));
 
   void changeKomaPointToRight(Koma koma) {
     final point = koma.point;
-    final newPoint = Point(x: point.x + 1, y: point.y);
 
-    print(koma.height);
-    print(koma.width);
-    print("------9999");
-    print(koma.komaType);
-    print(koma.title);
-    print(koma.id);
-    print(state[0].id);
-    print(koma.id == state[0].id);
-    print(state[1].id);
-    print("------9999");
-    state = [
-      for (final currentKoma in state)
+    // 空いているか&壁でないか
+    final canToRight = state.canToRight(koma);
+    print(canToRight);
+    print('========cantoright');
+    if (!canToRight) {
+      return;
+    }
+    final newPoint = Point(x: point.x + 1, y: point.y);
+    final newKomaList = [
+      for (final currentKoma in state.komaList)
         if (koma.id == currentKoma.id)
           Koma(id: koma.id, komaType: koma.komaType, title: koma.title, point: newPoint)
         else
           currentKoma,
     ];
-    print(state);
-    print(state[0].title);
-    print(state[1].title);
+    state = KomaMap(komaList: newKomaList);
   }
 }
 
