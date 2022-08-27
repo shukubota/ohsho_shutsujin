2a456c3283f17061f8ab776848d4ceeefd2a62ed
shukubota <kubotashu0421@gmail.com>
2022-08-27 14:59:49 +0900

    filterメソッド

--- lib/service/map.dart
+++ lib/service/map.dart
@@ -1,36 +1,52 @@
 import 'package:ohsho_shutsujin/model/koma.dart';
+import 'package:collection/collection.dart';
 
 const mapHeight = 5;
 const mapWidth = 4;
 
-
-
 class MapService {
   // komaオブジェクトのlist
   List<Koma> komaList = [];
-  List<Point> emptyPointList = [];
   MapService() {
-    final _emptyPointList = <Point>[];
-    for (int i = 0; i < mapHeight; i++) {
-      for (int j = 0; j < mapWidth; j++) {
-        _emptyPointList.add(Point(x: j, y: i));
-      }
-    }
-    emptyPointList = _emptyPointList;
+    // filledPointList = [];
+  }
+
+  init() {
+    // final ohsho = Koma(komaType: KomaType.ohsho);
+    // final kin = Koma(komaType: KomaType.kin);
+    // addKoma(ohsho);
+    // addKoma(kin);
   }
 
   addKoma(Koma koma) {
     // 位置が空いているかを確かめる コマの座標がすでに指定されていてvalidateする
-
-
-    komaList.add(koma);
+    final addPointList = koma.getPointList();
+    final filledPointList = getFilledPointList();
+    for (int i = 0; i < addPointList.length; i++) {
+      final addPoint = addPointList[i];
+      final isFilled = filledPointList.firstWhereOrNull((element) => element.isEqual(addPoint));
+      // すでにその座標が埋まっていればerror
+      if (isFilled != null) {
+        throw Error();
+      }
+    }
+    komaList = [...komaList, koma];
+    return true;
   }
 
-  List<Point> getEmptyPointList() {
+  List<Point> getFilledPointList() {
+    var _filledPointList = <Point>[];
     for (int i = 0; i < komaList.length; i++) {
       final Koma koma = komaList[i];
       final pointList = koma.getPointList();
+      for (int j = 0; j < pointList.length; j++) {
+        final point = pointList[j];
+        final existing = _filledPointList.firstWhereOrNull((e) => e.isEqual(point));
+        if (existing != null) {
+          _filledPointList.add(point);
+        }
+      }
     }
+    return _filledPointList;
   }
-
 }
