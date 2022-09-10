9b6d2156fdc5106eb2fd0b2e5a9314a07faf679b
shukubota <kubotashu0421@gmail.com>
2022-09-11 01:54:04 +0900

    firebaseの通知handler

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,78 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:ohsho_shutsujin/pages/form_example.dart';
 import 'package:ohsho_shutsujin/pages/panel.dart';
 
-void main() {
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print(message);
+  print("==background");
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("--------------aaa");
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  // NotificationSettings settings = await messaging.requestPermission(
+  //   alert: true,
+  //   announcement: false,
+  //   badge: true,
+  //   carPlay: false,
+  //   criticalAlert: false,
+  //   provisional: false,
+  //   sound: true,
+  // );
+
+  await messaging.setForegroundNotificationPresentationOptions(
+    alert: true, // Required to display a heads up notification
+    badge: true,
+    sound: true,
+  );
+
+  // print('User granted permission: ${settings.authorizationStatus}');
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print(fcmToken);
+  print('token');
+
+  // this.senderId,
+  // this.category,
+  // this.collapseKey,
+  // this.contentAvailable = false,
+  // this.data = const <String, dynamic>{},
+  // this.from,
+  // this.messageId,
+  // this.messageType,
+  // this.mutableContent = false,
+  // this.notification,
+  // this.sentTime,
+  // this.threadId,
+  // this.ttl
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+    print(message.senderId);
+    print(message.category);
+    print(message.notification?.title);
+    print(message.notification?.body);
+
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const ProviderScope(child: OhshoShutsujin()));
 }
 
